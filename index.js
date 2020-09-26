4d769549826ac1161e08be16ff740fd82f0d6466
mainulsakib <losersakib@gmail.com>
2020-09-26 10:41:01 +0600

    my first node project

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/',(req,res)=>{
+    res.send('I know how  to start node .... yay')
+})
+
+app.listen(3000,()=>console.log('listening to port 3000')) 
